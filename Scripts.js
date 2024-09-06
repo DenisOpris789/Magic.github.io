1d2cbc9df51ad739b5c12ebcbee6cc76f3f8a595
DenisOpris789 <denis.opris.gabriel@gmail.com>
2024-09-06 10:00:49 +0300

    Mersi
    Mersi

--- Scripts.js
+++ Scripts.js
@@ -2,3 +2,10 @@ function changeMenu(filename) {
     let img = document.querySelector('#Menuimg');
     img.setAttribute('src', filename);
 }
+
+var submitted = false;
+
+$('#gform').on('submit', function(e) {
+    location.reload();
+  });
+  
