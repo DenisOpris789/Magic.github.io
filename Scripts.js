887b5f7ce779bebe8f3f8ac0cd87fdee0f7d7426
DenisOpris789 <denis.opris.gabriel@gmail.com>
2024-09-09 00:27:11 +0300

    Final
    FInal

--- Scripts.js
+++ Scripts.js
@@ -3,28 +3,12 @@ function changeMenu(filename) {
     img.setAttribute('src', filename);
 }
 
-var submitted = false;
-$(document).ready(function() {
-    $('#gform').on('submit', function(e) {
-        e.preventDefault(); // Previne comportamentul default de trimitere a formularului
 
-        var $submitButton = $('#Trimis');
+var submitted=false;
 
-        // Schimbă textul butonului
-        $submitButton.val('Se trimite...');
-        $submitButton.prop('disabled', true);
-
-        // Afișează un mesaj de confirmare
-        $(this).prepend('<p class="confirm-message">Cererea a fost trimisă!</p>');
-
-        // Simulează trimiterea formularului (în caz că folosești un iframe ascuns)
-        setTimeout(function() {
-            // Resetează formularul
-            $('#gform')[0].reset();
-
-            // Reîncărcare pagină după ce mesajul a fost afișat pentru un timp
-            location.reload(true);
-        }, 2000); // 2 secunde întârziere
+$('#gform').on('submit', function(e) {
+    $('#gform *').fadeOut(2000);
+    $('#gform').prepend('Your submission has been processed...');
+    location.reload();
     });
-});
-
+    
