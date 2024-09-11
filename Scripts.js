1a69176c5f3b56ef7f192e90a34cb6882d9ab91b
DenisOpris789 <denis.opris.gabriel@gmail.com>
2024-09-11 09:03:20 +0300

    Final Final

--- Scripts.js
+++ Scripts.js
@@ -4,18 +4,3 @@ function changeMenu(filename) {
 }
 
 
-var submitted = false;
-
-$('#gform').on('submit', function(e) {
-    e.preventDefault(); // Previne trimiterea și reîncărcarea implicită a paginii.
-    $('#gform *').fadeOut(2000); // Animația de fade out
-    $('#gform').prepend('Your submission has been processed...');
-
-    // Așteaptă câteva secunde înainte de a reîncărca pagina
-    setTimeout(function() {
-        submitted = true;
-        $('#gform').submit(); // Trimiterea efectivă a formularului
-        location.reload(); // Reîncarcă pagina după trimitere
-    }, 2000);
-});
-
